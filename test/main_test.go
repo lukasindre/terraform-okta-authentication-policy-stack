22882cbca4c88fdc5885cf8a61f98a185a5a4c5a
Lukas <lukas.indre@upstart.com>
2024-12-16 10:50:12 -0500

    feat!: change module reads from YAML to Terraform Object
    YAML wouldn't allow us to leverage the full capability of Terraform.
    One use case would be extracting resource IDs upon creation, and using
    them somewhere else (e.g., a network zone is created via Terraform, and
    you can access the id by `okta_network_zone.zone.id`).
    
    Terraform is able to handle resource dependencies far better with this
    configuration, avoiding race conditions.

--- test/main_test.go
+++ test/main_test.go
@@ -1,39 +1,31 @@
 package test
 
 import (
-	"fmt"
-	"os"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
-    "gopkg.in/yaml.v3"
 )
 
-type Config struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Rules       []struct {
-		Name             string   `json:"name"`
-		Access           string   `json:"access"`
-		NetworkZones     []string `json:"network_zones"`
-		GroupTargets     []string `json:"group_targets"`
-		GroupExclusions  []string `json:"group_exclusions"`
-		ManagedDevice    []string `json:"managed_device"`
-		UnmanagedDevice  []string `json:"unmanaged_device"`
-		AuthFrequency    string   `json:"auth_frequency"`
-		StepUpAuth       bool     `json:"step_up_auth"`
-		PhishingResistant bool    `json:"phishing_resistant"`
-	} `json:"rules"`
-}
-
 func TestTerraformOktaAppSignonPolicy(t *testing.T) {
-    testFile := "./test.yaml"
-    fmt.Println("Test file: ", testFile)
 	tfOptions := &terraform.Options{
 		TerraformDir: "../",
 		Vars: map[string]interface{}{
-			"yaml_filepath": testFile,
+			"name": "test policy",
+			"description": "test policy description",
+			"rules": []map[string]interface{}{
+                {
+                    "name": "test rule",
+                    "access": "ALLOW",
+                    "network_zones": []string{"nzo98czx2D4vRb6VQ1d5", "nzodbozfphekSB8kz1d7"},
+                    "step_up_auth": true,
+                    "auth_frequency": "daily",
+                    "group_targets": []string{"00gj0yu1ezTEHw1w81d7"},
+                    "group_exclusions": []string{"00g2p34020fhFdSKE1d7"},
+                    "unmanaged_device": []string{"ios", "windows"},
+                    "phishing_resistant": true,
+			    },
+			},
 		},
 	}
 
@@ -43,18 +35,9 @@ func TestTerraformOktaAppSignonPolicy(t *testing.T) {
 	policyID := terraform.Output(t, tfOptions, "authentication_policy_id")
 	assert.NotEmpty(t, policyID, "Policy ID should not be empty")
 
-	configFile, err := os.ReadFile(testFile)
-	assert.NoError(t, err, "Failed to read config file")
+	policyRules := terraform.OutputList(t, tfOptions, "authentication_policy_rules")
+	assert.Equal(t, len(policyRules), 1, "Policy rules should be 1")
 
-	var config Config
-	err = yaml.Unmarshal(configFile, &config)
-	assert.NoError(t, err, "Failed to parse config file")
-
-    assert.Equal(t, len(config.Rules), 1, "Rules should not be empty")
-
-    groups := terraform.OutputList(t, tfOptions, "groups")
-    assert.Equal(t, len(groups), 2, "There should be 2 Groups.")
-
-    networkZones := terraform.OutputList(t, tfOptions, "network_zones")
-    assert.Equal(t, len(networkZones), 1, "There should be 1 Network Zone.")
+	networkZones := terraform.OutputList(t, tfOptions, "network_zones")
+	assert.Equal(t, len(networkZones), 2, "Network zones should be 2")
 }
