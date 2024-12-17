8141e639d791bcc19c60c6cf0252edc50b5db6c5
Lukas <lukas.indre@upstart.com>
2024-12-17 11:55:11 -0500

    fix: restructure examples

--- examples/basic_example.tf
+++ examples/basic_example.tf
@@ -1,3 +1,16 @@
+# Basic Example
+# This is a basic example of this module.
+#
+# We create a network zone and a group to use in the module.
+#
+# The module itself creates the following:
+# - An authentication Policy
+# - An implicit deny policy rule at priority 98
+# - An access allow policy for the following criteria
+#   - Traffic originating from the United States via Network Zone
+#   - User authentication a part of `Example Group One`
+#   - User is using an iOS device of any state (does not have to be managed, registered, etc.)
+
 resource "okta_network_zone" "example_us_zone" {
   type = "DYNAMIC"
   dynamic_locations = [
